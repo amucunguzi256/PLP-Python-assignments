00a4eb64cc1c5681b0d03dfce7d3d5fcebf0dcca
Aston Astex <astonastex@gmail.com>
2025-08-05 11:41:19 +0300

    Initial Commit - This is mu Assignment 2

---
+++ my_list.py
@@ -0,0 +1,10 @@
+
+my_list = []
+numbers= [10,20,30,40]
+for i in numbers:
+    my_list.append(i)
+my_list.insert(2,15)
+my_list.extend([50,60,70])
+my_list.pop()
+my_list.sort()
+print(my_list.index(30))
